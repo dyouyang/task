a2faefa52479df31ea86f77f99054dcd8cd63e3d
dyouyang <douyang@gmail.com>
2013-08-24 11:28:38 -0700

    Task detail activity now reads in the task data corresponding to the tapped item

--- Protaskinate/src/com/douyang/protaskinate/DetailActivity.java
+++ Protaskinate/src/com/douyang/protaskinate/DetailActivity.java
@@ -1,19 +1,63 @@
 package com.douyang.protaskinate;
 
+import com.douyang.contentprovider.MyTaskContentProvider;
+import com.douyang.db.TaskTable;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.database.Cursor;
 import android.view.Menu;
 import android.widget.EditText;
 
 public class DetailActivity extends Activity {
 
+	private Uri taskUri;
+	EditText edit_text;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detail);
 		
-		EditText edit_text = (EditText) findViewById(R.id.editText1);
+		edit_text = (EditText) findViewById(R.id.editText1);
 		
+		Bundle extras = getIntent().getExtras();
+		
+		//Get from saved state
+		if(savedInstanceState == null)
+			taskUri = null;
+		else
+			taskUri = savedInstanceState.getParcelable(MyTaskContentProvider.CONTENT_ITEM_TYPE);
+		
+		//get from other activity
+		if(extras != null)
+		{
+			taskUri = extras.getParcelable(MyTaskContentProvider.CONTENT_ITEM_TYPE);
+			fillData(taskUri);
+		}
+		
+		
+	}
+
+	
+	
+	/**
+	 * fills the task fields on this activity with the task clicked on list activity
+	 * @param uri
+	 */
+	private void fillData(Uri uri) {
+		String [] projection = { TaskTable.COLUMN_SUMMARY, TaskTable.COLUMN_DESCRIPTION, TaskTable.COLUMN_CATEGORY };
+		Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+		
+		if(cursor != null)
+		{
+			cursor.moveToFirst();
+			edit_text.setText(cursor.getString(cursor.getColumnIndexOrThrow(TaskTable.COLUMN_SUMMARY)));
+		}
+		
+		cursor.close();
+
 	}
 
 	@Override
@@ -23,4 +67,4 @@ public class DetailActivity extends Activity {
 		return true;
 	}
 
-} 
+}
